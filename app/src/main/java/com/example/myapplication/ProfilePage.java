66048e74572314f18f1264b440564373a2c677df
none9on <meet.u.in.tokyo@gmail.com>
2024-09-21 21:27:07 +0300

    groovy as a build language instead of kotlin, firebase пока конектится но страшно, что завтра всё упадет

---
+++ app/src/main/java/com/example/myapplication/ProfilePage.java
@@ -0,0 +1,44 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ProfilePage extends AppCompatActivity {
+    private Button movethree;
+    private Button movefour;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_page);
+        movethree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfilePage.this, SearchPage.class);
+                startActivity(intent);
+                overridePendingTransition(0, 0);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                finish();
+            }
+        });
+        ImageButton movefour = (ImageButton) findViewById(R.id.home_page_notbold);
+        movefour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfilePage.this, MainActivity.class);
+                startActivity(intent);
+                overridePendingTransition(0, 0);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                finish();
+            }
+        });
+    }
+}
